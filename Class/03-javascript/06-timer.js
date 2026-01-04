e43e1f9d624bb8a551f3418dda7bb57af71aaf83
24JunHyeok <wsxqaz6126@gmail.com>
2026-01-04 18:57:57 +0900

    0104_2

--- Class/03-javascript/06-timer.js
+++ Class/03-javascript/06-timer.js
@@ -1,18 +1,29 @@
-
+let isStarted = false;
 
 const cl = ()=>{
-    let rand = String(Math.floor(Math.random()*1000000)).padStart(6,"0");
-    document.getElementById("num").innerText = rand;
-    let time = 180;
-    setInterval(function(){
-        if(time>=0){
-            let min = Math.floor(time/60);
-            let sec = String(time%60).padStart(2,"0");
-            document.getElementById("timer").innerText = `${min}:${sec} `
-            time = time-1;
-        }else {
-            document.getElementById("ch").disabled=true
-        }
-    },1000)
+    if(isStarted===false){
+        //타이머가 작동 중이 아닐때
+        isStarted = true
+        document.getElementById("ch").disabled = false
+        let rand = String(Math.floor(Math.random()*1000000)).padStart(6,"0");
+        document.getElementById("num").innerText = rand;
+        let time = 5;
+        let timer
+        timer = setInterval(function(){
+            if(time>=0){
+                let min = Math.floor(time/60);
+                let sec = String(time%60).padStart(2,"0");
+                document.getElementById("timer").innerText = `${min}:${sec} `
+                time = time-1;
+            }else {
+                document.getElementById("ch").disabled=true
+                isStarted = false
+                clearInterval(timer)
+            }
+        },1000)
 
+    }else {
+        //타이머가 작동 중일때
+    }
+    
 }
