837ff9aac1a8272c4db2027fa7d96260b54ea3b7
24JunHyeok <wsxqaz6126@gmail.com>
2026-01-04 16:49:24 +0900

    0104_1

---
+++ Class/03-javascript/05-function.js
@@ -0,0 +1,9 @@
+// const number = ()=>{
+//     let num = document.getElementById("num");
+//     num.innerText = String(Math.floor(Math.random()*1000000)).padStart(6,"0");
+// }
+let auth = ()=>{
+    const token = String(Math.floor(Math.random()*1000000)).padStart(6,"0")
+    document.getElementById("target").innerText = token;
+    document.getElementById("target").style.color = "#"+token;
+}
