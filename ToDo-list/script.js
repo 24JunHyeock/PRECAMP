05066d4dc2d81c291493cb0616846f0c4bb5686c
24JunHyeok <wsxqaz6126@gmail.com>
2026-01-13 16:08:00 +0900

    0113_4

---
+++ ToDo-list/script.js
@@ -0,0 +1,62 @@
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = (storageData) => {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contains;
+  }
+  const newLi = document.createElement("Li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+    saveItemsFn();
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+  if (storageData.complete) {
+    newLi.classList.add("complete");
+  }
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFn();
+};
+const keyCodeCheck = () => {
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createTodo();
+  }
+};
+const deleteAll = () => {
+  const liList = document.querySelectorAll("Li");
+  //   for (let i = 0; i < liList.length; i++) {
+  //     liList[i].remove();
+  //   }
+  for (let x of liList) {
+    x.remove();
+  }
+};
+const saveItemsFn = () => {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains,
+    };
+    saveItems.push(todoObj);
+  }
+  localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
