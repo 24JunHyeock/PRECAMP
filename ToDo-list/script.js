3afe3fddfb792980c55fd79595ec0aa8939fabf6
24JunHyeok <wsxqaz6126@gmail.com>
2026-01-14 17:40:41 +0900

    0114_1

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -2,6 +2,7 @@ const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
 const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
 
 const createTodo = (storageData) => {
   let todoContents = todoInput.value;
@@ -32,7 +33,7 @@ const createTodo = (storageData) => {
   saveItemsFn();
 };
 const keyCodeCheck = () => {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     createTodo();
   }
 };
@@ -65,3 +66,79 @@ if (savedTodoList) {
     createTodo(savedTodoList[i]);
   }
 }
+
+const weatherDataActive = ({ location, weather }) => {
+  const weatherMainList = [
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Thunderstorm",
+  ];
+  weather = weatherMainList.includes(weather) ? weather : "Fog";
+  const locationNameTag = document.querySelector("#location-name-tag");
+  locationNameTag.textContent = location;
+  document.body.style.backgroundImage = `url(./images/${weather}.jpg)`;
+
+  if (
+    !savedWeatherData ||
+    savedWeatherData.location !== location ||
+    savedWeatherData.weather !== weather
+  ) {
+    localStorage.setItem(
+      "saved-weather",
+      JSON.stringify({ location, weather })
+    );
+  }
+};
+
+const weatherSearch = ({ latitude, longitude }) => {
+  const openWeatherRes = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=32d0fe83e8da903a1b63bbde58621c4a`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].main);
+      const weatherData = {
+        location: json.name,
+        weather: json.weather[0].main,
+      };
+      weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+      console.err(err);
+    });
+};
+
+const accessToGeo = ({ coords }) => {
+  const { latitude, longitude } = coords;
+  //shorthand property
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+  weatherSearch(positionObj);
+};
+
+const askForLocation = () => {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
+};
+askForLocation();
+// const promiseTest = () => {
+//   return new Promise((resolver, reject) => {
+//     setTimeout(() => {
+//       resolver("success");
+//       // reject("error");
+//     }, 5000);
+//   });
+// };
+// console.log(promiseTest());
+
+// promiseTest().then((res) => {
+//   console.log(res);
+// });
